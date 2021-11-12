c3e1539ec3bf0e7e198b8ae20326b013a79dae89
Marina Iv <marisha@marinas-mbp.myfiosgateway.com>
2021-11-11 19:07:03 -0500

    lesson 1.4 - lifting state and props handling

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,23 +1,31 @@
 import React from 'react';
 
 
-function AddTodoForm(props) {
+function AddTodoForm({onAddTodo}) {
+
+    const [todoTitle, setTodoTitle] = React.useState("");
+
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    };
 
     const handleAddToDo = (event) => {
         event.preventDefault();
-        const todoTitle = event.target.todoTitle.value;
-        console.log(todoTitle);
+        // const todoTitle = event.target.todoTitle.value;
+        // console.log(todoTitle);
         // console.log(event);
-        props.onAddTodo(todoTitle);
+        onAddTodo({title: todoTitle, id: Date.now()});
         // todoTitle.reset(); resetting the form
-        event.target.todoTitle.value = '';
+        // event.target.todoTitle.value = '';
+        setTodoTitle('');
     };
 
     return(
         <div>
             <form onSubmit={handleAddToDo} >
                 <label htmlFor="todoTitle">Title </label>
-                <input id="todoTitle" name="title" type="text"></input>
+                <input id="todoTitle" name="title" type="text" value={todoTitle} onChange={handleTitleChange}></input>
                 <button>Add</button>
             </form>         
         </div>
