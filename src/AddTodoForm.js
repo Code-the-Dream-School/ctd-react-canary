afe9d4c71693b5988a3f70e67b015f71e95d920e
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-11-12 06:28:52 -0800

    Add todo dynamically from the browser

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
+function AddTodoForm({ onAddTodo }) {
+	const [todoTitle, setTodoTitle] = useState();
 
+	function handleTitleChange(e) {
+		const newTodoTitle = e.target.value;
+		setTodoTitle(newTodoTitle);
+	}
 
-function AddTodoForm( {onAddTodo} ) {
-  function handleAddTodo(event){
-    event.preventDefault();
-    const todoTitle = event.target.title.value;
-    event.target.title.value =""; 
-    onAddTodo(todoTitle);
-  }
+	function handleAddTodo(e) {
+		e.preventDefault();
+		onAddTodo({ title: todoTitle, id: Date.now() });
+		setTodoTitle("");
+	}
 
-  return (
-    <div>
-      <form onSubmit={handleAddTodo}>
-        <label htmlFor="todoTitle">Title</label>
-        <input id="todoTitle" name="title"></input>
-        <button>Add</button>
-      </form>
-      
-    </div>
-  )
+	return (
+		<div>
+			<form onSubmit={handleAddTodo}>
+				<label htmlFor="todoTitle">Title</label>
+				<input
+					value={todoTitle}
+					onChange={handleTitleChange}
+					id="todoTitle"
+					name="title"
+				></input>
+				<button>Add</button>
+			</form>
+		</div>
+	);
 }
 
 export default AddTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,34 +1,14 @@
-import React from 'react';
-import TodoListItem from './TodoListItem';
+import React from "react";
+import TodoListItem from "./TodoListItem";
 
-const todoList = [
-  {
-    id: 1,
-    title: "Complete assignment",
-    dueDate: "Tuesday",
-  },
-  {
-    id: 2,
-    title: "Read React official documents",
-    dueDate: "Saturday",
-  },
-  {
-    id: 3,
-    title: "Clean the car",
-    dueDate: "ASAP",
-  }
-]
-
-function TodoList(){
-  return(
-    <ul>
-        {todoList.map((item) => {
-          return (
-            <TodoListItem key={item.id} item={item}/>
-          )
-        })}
-      </ul>
-  )
+function TodoList({ todoList }) {
+	return (
+		<ul>
+			{todoList.map((item) => {
+				return <TodoListItem key={item.id} item={item} />;
+			})}
+		</ul>
+	);
 }
 
 export default TodoList;
