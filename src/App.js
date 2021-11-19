99d10f4fc6b497086a3e74cf50b957fd6063af04
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-11-19 06:07:00 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const[newTodo, setNewTodo] = useState()
-
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo}/>
-      <p>{newTodo}</p>
-      <TodoList />
-    </div>
-  );
+	const [newTodo, setNewTodo] = useState();
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<AddTodoForm onAddTodo={setNewTodo} />
+			<p>{newTodo}</p>
+			<TodoList />
+		</div>
+	);
 }
 
 export default App;
