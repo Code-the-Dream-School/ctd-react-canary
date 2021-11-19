373a7815664ccf88df2675e6886010e38a188733
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-11-19 10:29:19 -0800

    Revert "lesson 1.5"
    This reverts commit 503654b5e01a97daa334852c98c5794fe3cf829b.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-function useSemiPersistentState() {
-	const [todoList, setTodoList] = useState(
-		JSON.parse(localStorage.getItem("savedTodoList"))
-	);
-	useEffect(() => {
-		localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-	}, [todoList]);
-
-	return [todoList, setTodoList];
-}
-
 function App() {
-	const [todoList, setTodoList] = useSemiPersistentState();
+	const [todoList, setTodoList] = useState([]);
 
 	function addTodo(newTodo) {
 		setTodoList([...todoList, newTodo]);
 	}
 
 	return (
-		<>
+		<div>
 			<h1>Todo List</h1>
 			<AddTodoForm onAddTodo={addTodo} />
 
 			<TodoList todoList={todoList} />
-		</>
+		</div>
 	);
 }
 
