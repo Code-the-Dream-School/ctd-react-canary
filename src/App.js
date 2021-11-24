14a11f2a74ac7a1e70c0c4a0e1c0fcc9fea29436
kelvin <kiruikkev@gmail.com>
2021-11-23 19:16:05 -0500

    lesson-1-5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
- 
 
-    const[todoList, setTodoList]= React.useState([]);
+    const[todoList, setTodoList]= React.useState(JSON.parse(localStorage.getItem("savedTodoList"))||[]);
+
+    useEffect(()=>{
+      localStorage.setItem("savedTodoList",JSON.stringify(todoList));
+     },[todoList]
+    )
+
     const addTodo =(newTodo)=>{
-      console.log(newTodo);
- setTodoList([...todoList,newTodo]);
+     setTodoList([...todoList,newTodo]);
       
-    }
+   }
   return (
     <div>
       <h1> TodoList </h1>
