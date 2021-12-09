a3819e8af753407086158fb7989729f2ff5dac6a
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-12-09 12:11:39 -0800

    Async data fetching mimic

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-function useSemiPersistentState() {
-	const [todoList, setTodoList] = useState(
-		JSON.parse(localStorage.getItem("savedTodoList"))
-	);
-	useEffect(() => {
-		localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-	}, [todoList]);
-
-	return [todoList, setTodoList];
-}
-
 function App() {
-	const [todoList, setTodoList] = useSemiPersistentState();
+	const [todoList, setTodoList] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() =>
+		new Promise((resolve, reject) =>
+			setTimeout(
+				() =>
+					resolve({
+						data: {
+							todoList: JSON.parse(localStorage.getItem("savedTodoList")),
+						},
+					}),
+				2000
+			)
+		).then((result) => {
+			setTodoList(result.data.todoList);
+			setIsLoading(false);
+		})
+	);
+
+	useEffect(() => {
+		if (isLoading === false) {
+			localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+		}
+	}, [todoList]);
 
 	function addTodo(newTodo) {
 		setTodoList([...todoList, newTodo]);
@@ -29,8 +42,11 @@ function App() {
 		<>
 			<h1>Todo List</h1>
 			<AddTodoForm onAddTodo={addTodo} />
-
-			<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+			{isLoading ? (
+				<p>Loading..</p>
+			) : (
+				<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+			)}
 		</>
 	);
 }
