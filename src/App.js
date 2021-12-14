528f6cf21cc244efe130e5151a0405cffe44d6ba
Kenneth <kenldurham@gmail.com>
2021-12-13 16:51:23 -0800

    Lesson-1-7

--- src/App.js
+++ src/App.js
@@ -2,36 +2,49 @@ import React from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
-  React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-  //console.log(localStorage);
-  //React.useEffect(() => {
-  // setTodoList(JSON.parse(localStorage.getItem("savedTodoList")));
-  //}, []);
-
-  return [todoList, setTodoList];
-};
+//console.log(localStorage);
+//React.useEffect(() => {
+//setTodoList(JSON.parse(localStorage.getItem("savedTodoList")));
+//}, []);
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(
+        () =>
+          resolve({
+            data: {
+              todoList: JSON.parse(localStorage.getItem("savedTodoList")),
+            },
+          }),
+        2000
+      );
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
   const removeTodo = (id) => {
     const newTodo = todoList.filter((item) => {
-     return item.id !== id 
-     
-    })
+      return item.id !== id;
+    });
     setTodoList(newTodo);
   };
   return (
     <>
       <h1>Todo List</h1>
+      {isLoading ? <p>Loading...</p> : [TodoList]}
       <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
       <AddTodoForm onAddTodo={addTodo} />
       <p></p>
